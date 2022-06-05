2082330025919f074b3e178b81b9ce0b9ff6a028
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-05 11:38:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+//Remove main
+const weremoveMain = document.getElementById('main');
+
+weremoveMain.remove();
+
+//NewHeader 'h1#victory'
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+
+//newHeader variable that points to node 'h1#victory'
+
+newHeader.innerHTML = "Elvis Munene is the champion";
